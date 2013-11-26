b7fec6ab97b772d0240176d8898b74e5508eff41
DarkBoss <darkboss@PCMorgan.(none)>
2013-11-26 11:38:04 +0100

    Added strlen test

---
+++ tests/strlen/main.c
@@ -0,0 +1,35 @@
+#include "libft.h"
+#include "common.h"
+#include <string.h>
+#include <ctype.h>
+
+#define FUNC_NAME "strlen"
+
+void	do_test(char const *a)
+{
+	FILE	*stream;
+	size_t	ra;
+	size_t	rb;
+
+	ra = ft_strlen(a);
+	rb = strlen(a);
+	stream = ra == b ? stdout : stderr;
+	fprintf(stream,
+			"Len of '%s': libft: %i - libc: %i\n",
+			a,
+			(int)ra,
+			(int)rb);
+}
+
+int	main(void)
+{
+	print_hr();
+	printf("Testing '%s':\n", FUNC_NAME);
+	print_hr();
+
+	do_test("Bite");
+	do_test("", "Molle");
+
+	print_hr();
+	return (0);
+}
