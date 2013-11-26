8c67b0c0219f20c0b860939710ca5c00f757486e
Morgan WELSCH <mwelsch@e1r10p17.42.fr>
2013-11-26 15:04:56 +0100

    test merge

--- src/ft_strchr.c
+++ src/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: mwelsch <mwelsch@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/20 18:53:40 by mwelsch           #+#    #+#             */
-/*   Updated: 2013/11/20 20:06:16 by mwelsch          ###   ########.fr       */
+/*   Updated: 2013/11/26 14:40:57 by mwelsch          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ char	*ft_strchr(const char *str, int c)
 			return ((char*)str);
 		str ++;
 	}
-	if (c == '\0' && *str == c)
+	if (str && c == '\0' && *str == c)
 		return ((char*)str);
 	return (NULL);
 }
