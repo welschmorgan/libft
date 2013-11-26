be3cde47a2c408710785181ae773f0dbc151095c
Morgan WELSCH <mwelsch@e1r10p17.42.fr>
2013-11-26 20:15:21 +0100

    update

--- src/ft_strsub.c
+++ src/ft_strsub.c
@@ -6,7 +6,7 @@
 /*   By: mwelsch <mwelsch@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/22 21:06:13 by mwelsch           #+#    #+#             */
-/*   Updated: 2013/11/22 22:20:10 by mwelsch          ###   ########.fr       */
+/*   Updated: 2013/11/26 20:09:46 by mwelsch          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,19 @@ char	*ft_strsub(char const *s1, unsigned int start, size_t len)
 	char	*ret;
 
 	slen = ft_strlen(s1);
-	if (((start + len) >= slen) || len == 0)
+	if (((start + len) > slen) || len == 0)
 		return (NULL);
-	ret = ft_strnew(start + len);
+	ret = ft_strnew(len);
 	if (!ret)
 		return (NULL);
 	p_s1 = (char *)s1;
 	p_ret = ret;
-	while (len --)
+	while (start)
+	{
+		p_s1++;
+		start--;
+	}
+	while (len--)
 	{
 		*(p_ret++) = *(p_s1++);
 	}
