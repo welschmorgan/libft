e55f09912ca9a73d4ae1c72e910306b73d345786
Morgan WELSCH <mwelsch@e3r12p15.42.fr>
2016-03-21 15:25:52 +0100

    rendu: nearly there...

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: mwelsch <mwelsch@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/11/23 18:23:08 by mwelsch           #+#    #+#             */
-/*   Updated: 2016/03/21 14:42:31 by mwelsch          ###   ########.fr       */
+/*   Updated: 2016/03/21 14:51:54 by mwelsch          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,9 @@ t_list			*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 	p_cur = lst;
 	while (p_cur)
 	{
-		printf("map: %p (%u)", p_cur, (unsigned)p_cur->content_size);
 		ft_lstadd(&p_dup, f(p_cur));
 		if (!p_dup)
 			return (NULL);
-		printf(" -> %p (%u)\n", p_dup, (unsigned)p_dup->content_size);
 		p_cur = p_cur->next;
 	}
 	return (p_dup);
